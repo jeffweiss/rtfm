1009cbcb5c22547fda63d4644a9bd4104576f299
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-05-23 23:13:13 -0700

    add queue publishing to submit daemon

--- submit_daemon/watcher.rb
+++ submit_daemon/watcher.rb
@@ -5,12 +5,20 @@ notifier = INotify::Notifier.new
 
 dir = "/tmp/facts/spool"
 
+stomp_login_hash = {
+  :hosts => [ { :login => ARGV[0], :passcode => ARGV[1], :host => ARGV[2], :port => 61613, :ssl => true}]
+}
+client = Stomp::Client.new(stomp_login_hash)
+queue_name = "/queue/rtfm"
+
 notifier.watch(dir, :moved_to, :close_write) do |event|
   filename = File.join(dir, event.name)
   begin
     contents = File.read(filename)
     contents_as_json = JSON.parse(contents)
-    puts "retrieved facts from #{contents_as_json['timestamp']}"
+    puts "retrieved facts from #{contents_as_json["timestamp"]}"
+    client.publish queue_name, contents, {:persistent => true}
+    puts "  -- published to #{queue_name}"
   rescue
     puts "error reading contents of #{filename}"
   end
