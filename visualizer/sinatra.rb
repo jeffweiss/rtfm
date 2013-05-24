ef08f05c0f2f6df88c1cf491bcec00d9154d011c
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-05-24 02:23:51 -0700

    add start of sinatra service to serve data

---
+++ visualizer/sinatra.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'pg'
+require 'json'
+
+db_conn = PG.connect( :dbname => 'rtfm', :user => 'rtfm', :password => 'rtfm', :host => ARGV[2])
+
+get '/fact/:fact' do |fact|
+  db_conn.exec('select extract(epoch from tstamp) as tstamp, value from facts where name = $1', [fact]).values.to_json
+end
