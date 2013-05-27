027c7eb20298a567860dfc5fb2c45db91cc0a03d
Jeff Weiss <jeff.weiss@puppetlabs.com>
2013-05-27 15:58:09 -0700

    add rudimentary barchart

--- visualizer/sinatra.rb
+++ visualizer/sinatra.rb
@@ -7,3 +7,7 @@ db_conn = PG.connect( :dbname => 'rtfm', :user => 'rtfm', :password => 'rtfm', :
 get '/fact/:fact' do |fact|
   db_conn.exec('select extract(epoch from tstamp) as tstamp, value from facts where name = $1', [fact]).values.to_json
 end
+
+get '/' do
+  erb :index, :locals => { :current_fact => "memoryfree_mb" }
+end
